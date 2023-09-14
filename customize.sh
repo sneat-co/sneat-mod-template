861e03e2852551347cd5e44ea7e91f4fa9075bf7
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-09-14 18:02:50 +0100

    fix: customize.sh - initial customizations for Go module

--- customize.sh
+++ customize.sh
@@ -3,9 +3,28 @@
 current_dir=$(basename "$(pwd)")
 MODULE_ID="${current_dir#sneat-module-}"
 echo "Module id: $MODULE_ID"
+
+# ======== BEGIN: Customization of Go related files & directories ========
+
+# Renames module name in go.mod file using actual module id taken from $MODULE_ID environment variable
+sed -i '' "s/sneat-mod-module/sneat-mod-$MODULE_ID/g" go.mod
+
+# Renames module sub-directories using actual module id taken from $MODULE_ID environment variable
+for dir in go/module/*module; do mv "$dir" "${dir%module}$MODULE_ID"; done
+
+# Changes package name "module" to actual module id taken from $MODULE_ID environment variable
+find "go/module" -type f -name "*.go" -exec sed -i '' "s/package module/package $MODULE_ID/g" {} +
+
+# Replace {MODULE_ID} with actual module id taken from $MODULE_ID environment variable
+find "go/module" -type f -name "*.go" -exec sed -i '' "s/{MODULE_ID}/$MODULE_ID/g" {} +
+
+
+# Renames module directory using actual module id taken from $MODULE_ID environment variable
 mv go/module go/"$MODULE_ID"
-sed -i '' "s/sneat-module-template/sneat-mod-$MODULE_ID/g" go.mod
-find go/module -type d -name "*module" -exec bash -c 'mv "$1" "${1%module}$MODULE_ID"' bash {} \;
-find "go" -type f -name "*.go" -exec sed -i '' "s/package module/package $MODULE_ID/g" {} +
-find "go" -type f -name "*.go" -exec sed -i '' "s/{MODULE_ID}/$MODULE_ID/g" {} +
+
+# ======== END: Customization of Go related files & directories ========
+
+# ======== BEGIN: Clean-up ========
+# Remove customization script
 rm customize.sh
+# ======== END: Clean-up ========
